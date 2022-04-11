76aa50e19c43f4a140c65040605f52c36f5c77b6
David Roundy <daveroundy@gmail.com>
2022-04-11 11:10:42 -0700

    make transcription work all right, even ignoring silences

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+
+use cpal::StreamConfig;
 
 pub fn transcription() {
-    let model = coqui_stt::Model::new("english/model.tflite").expect("unable to create model");
+    let mut model = coqui_stt::Model::new("english/model.tflite").expect("unable to create model");
+    model
+        .enable_external_scorer("english/huge-vocabulary.scorer")
+        .expect("unable to read scorer");
+    let model = Arc::new(model);
     let desired_rate = cpal::SampleRate(model.get_sample_rate() as u32);
-    let streaming = Arc::new(Mutex::new(
-        model.into_streaming().expect("trouble streaming the model"),
-    ));
+    // let streaming = Arc::new(Mutex::new(
+    //     model.into_streaming().expect("trouble streaming the model"),
+    // ));
 
     use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
     let host = cpal::default_host();
@@ -23,29 +29,97 @@ pub fn transcription() {
         .next()
         .expect("No input at desired sample rate")
         .with_sample_rate(desired_rate);
-    let config = supported_config.into();
-    let streaming_copy = streaming.clone();
+    let mut config: StreamConfig = supported_config.into();
+    let mut silent_buffers = 0;
+    let mut last_length = 0;
+    let latency_samples = 320 * desired_rate.0 as usize / 1000;
+    config.buffer_size = cpal::BufferSize::Fixed(latency_samples as u32);
+    let mut samples_taken = 0;
+    let mut streaming: Option<coqui_stt::Stream> = None;
+
+    // let streaming_copy = streaming.clone();
     let stream = device
         .build_input_stream(
             &config,
             move |data: &[i16], _: &cpal::InputCallbackInfo| {
-                // react to stream events and read or write stream data here.
-                streaming_copy.lock().unwrap().feed_audio(data);
+                if streaming.is_none() {
+                    streaming = Some(
+                        coqui_stt::Stream::from_model(model.clone())
+                            .expect("unable to create stream?!"),
+                    );
+                }
+                let mut finished_phrase = false;
+                if let Some(stream) = &mut streaming {
+                    samples_taken += data.len();
+                    stream.feed_audio(data);
+                    if samples_taken > latency_samples {
+                        samples_taken -= latency_samples;
+                        if let Ok(s) = stream.intermediate_decode() {
+                            if s.len() > 1 {
+                                if s.len() != last_length {
+                                    last_length = s.len();
+                                    silent_buffers = 0;
+                                } else {
+                                    silent_buffers += 1;
+                                    if silent_buffers > 3 {
+                                        finished_phrase = true;
+                                        println!("Said: {s}");
+                                        // for (n, t) in m.transcripts().iter().enumerate() {
+                                        //     let confidence = t.confidence();
+                                        //     let mut v = String::new();
+                                        //     let mut time = 0.0;
+                                        //     for s in t.tokens().iter() {
+                                        //         v.push_str(s.text().as_ref());
+                                        //         time = s.start_time();
+                                        //     }
+                                        //     println!("{n:2}: {confidence:5.3} {v:?} at {time:.2}");
+                                        // }
+                                    }
+                                }
+                            }
+                        }
+
+                        if finished_phrase {
+                            silent_buffers = 0;
+                            last_length = 0;
+                            let old = std::mem::replace(&mut streaming, None);
+                            drop(old);
+                        }
+                    }
+                }
             },
             move |err| {
                 // react to errors here.
-                panic!("stream error: {}", err)
+                panic!("stream error: {}", err);
             },
         )
         .expect("error creating stream");
     stream.play().unwrap();
-
     loop {
-        if let Ok(s) = streaming.lock().unwrap().intermediate_decode() {
-            if s.len() > 0 {
-                println!("Said: {s}");
-            }
-        }
-        std::thread::sleep(std::time::Duration::from_secs_f64(1.0));
+        std::thread::sleep(std::time::Duration::from_secs_f64(1.0e3));
     }
+
+    // loop {
+    //     if let Ok(m) = streaming
+    //         .lock()
+    //         .unwrap()
+    //         .intermediate_decode_with_metadata(10)
+    //     {
+    //         println!();
+    //         for (n, t) in m.transcripts().iter().enumerate() {
+    //             let confidence = t.confidence();
+    //             let mut v = String::new();
+    //             let mut time = 0.0;
+    //             for s in t.tokens().iter() {
+    //                 v.push_str(s.text().as_ref());
+    //                 time = s.start_time();
+    //             }
+    //             println!("{n:2}: {confidence:5.3} {v:?} at {time:.2}");
+    //         }
+    //         // if s.len() > 0 {
+    //         //     println!("Said: {s}");
+    //         // }
+    //     }
+    //     std::thread::sleep(std::time::Duration::from_secs_f64(1.0));
+    // }
 }
