eebadaaf17d58410248a3747b047d4bdc1af663f
David Roundy <daveroundy@gmail.com>
2022-04-12 04:48:49 -0700

    try using vad, untested because it is early morning

--- src/lib.rs
+++ src/lib.rs
@@ -115,6 +115,13 @@ pub fn voice_control() {
     //     model.into_streaming().expect("trouble streaming the model"),
     // ));
 
+    assert_eq!(model.get_sample_rate(), 16000);
+    let vad = std::sync::Mutex::new(webrtc_vad::Vad::new_with_rate_and_mode(
+        webrtc_vad::SampleRate::Rate16kHz,
+        webrtc_vad::VadMode::Quality,
+    ));
+    const VAD_SAMPLES: u32 = 16 * 30; // 30 ms at 16 kHz.  10 and 20 are also options.
+
     use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
     let host = cpal::default_host();
     let device = host
@@ -132,12 +139,12 @@ pub fn voice_control() {
         .expect("No input at desired sample rate")
         .with_sample_rate(desired_rate);
     let mut config: StreamConfig = supported_config.into();
-    let mut silent_buffers = 0;
-    let mut last_length = 0;
-    let latency_samples = 320 * desired_rate.0 as usize / 1000;
-    config.buffer_size = cpal::BufferSize::Fixed(latency_samples as u32);
-    let mut samples_taken = 0;
-    let mut streaming: Option<coqui_stt::Stream> = None;
+    config.buffer_size = cpal::BufferSize::Fixed(VAD_SAMPLES);
+
+    let mut have_sound = false;
+    let new_stream =
+        move || coqui_stt::Stream::from_model(model.clone()).expect("unable to create stream?!");
+    let mut stream = new_stream();
 
     let rules = Arc::new(std::sync::Mutex::new(parser::my_rules()));
     // let streaming_copy = streaming.clone();
@@ -145,95 +152,49 @@ pub fn voice_control() {
         .build_input_stream(
             &config,
             move |data: &[i16], _: &cpal::InputCallbackInfo| {
-                if streaming.is_none() {
-                    streaming = Some(
-                        coqui_stt::Stream::from_model(model.clone())
-                            .expect("unable to create stream?!"),
-                    );
-                }
-                let mut finished_phrase = false;
-                if let Some(stream) = &mut streaming {
-                    samples_taken += data.len();
+                if vad
+                    .lock()
+                    .unwrap()
+                    .is_voice_segment(data)
+                    .expect("wrong size data sample")
+                {
                     stream.feed_audio(data);
-                    if samples_taken > latency_samples {
-                        samples_taken -= latency_samples;
-                        if let Ok(s) = stream.intermediate_decode() {
-                            if s.len() > 1 {
-                                if s.len() != last_length {
-                                    last_length = s.len();
-                                    silent_buffers = 0;
-                                } else {
-                                    silent_buffers += 1;
-                                    if silent_buffers > 3 {
-                                        finished_phrase = true;
-                                        // let words = s.split_whitespace().collect::<Vec<_>>();
-                                        // let mut words = &words[..];
-                                        // while words.len() > 0 {
-                                        //     if let Some((a, rest)) =
-                                        //         rules.lock().unwrap().parse(words)
-                                        //     {
-                                        //         assert!(rest.len() < words.len());
-                                        //         words = rest;
-                                        //         a.run();
-                                        //     } else {
-                                        //         println!("Unrecognized: {words:?}");
-                                        //         break;
-                                        //     }
-                                        // }
-                                        let x = stream
-                                            .intermediate_decode_with_metadata(30)
-                                            .unwrap()
-                                            .to_owned();
-                                        let mut best = 0;
-                                        let mut best_vec = Vec::new();
-                                        for c in x.transcripts().iter() {
-                                            let mut words = String::new();
-                                            for w in c.tokens().iter().map(|t| &t.text) {
-                                                words.push_str(w.as_ref());
-                                            }
-                                            let original_words =
-                                                words.split_whitespace().collect::<Vec<_>>();
-                                            let mut words = &original_words[..];
-                                            let mut goodness = 0;
-                                            while let Some((_,rest)) = rules.lock().unwrap().parse(words) {
-                                                words = rest;
-                                                goodness += 1;
-                                            }
-                                            // println!("{goodness:2}: {original_words:?}");
-                                            if goodness > best {
-                                                best = goodness;
-                                                best_vec = original_words.iter().map(|w| w.to_string()).collect();
-                                            }
-                                        }
-                                        let words = best_vec.iter().map(|w| w.as_str()).collect::<Vec<_>>();
-                                        let mut words = &words[..];
-                                        while let Some((a,rest)) = rules.lock().unwrap().parse(&words[..]) {
-                                            assert!(rest.len() < words.len());
-                                            words = rest;
-                                            a.run();
-                                        }
-                                        // for (n, t) in m.transcripts().iter().enumerate() {
-                                        //     let confidence = t.confidence();
-                                        //     let mut v = String::new();
-                                        //     let mut time = 0.0;
-                                        //     for s in t.tokens().iter() {
-                                        //         v.push_str(s.text().as_ref());
-                                        //         time = s.start_time();
-                                        //     }
-                                        //     println!("{n:2}: {confidence:5.3} {v:?} at {time:.2}");
-                                        // }
-                                    }
-                                }
+                    have_sound = true;
+                } else {
+                    if have_sound {
+                        let x = std::mem::replace(&mut stream, new_stream())
+                            .finish_stream_with_metadata(32)
+                            .unwrap()
+                            .to_owned();
+                        let mut best = 0;
+                        let mut best_vec = Vec::new();
+                        for c in x.transcripts().iter() {
+                            let mut words = String::new();
+                            for w in c.tokens().iter().map(|t| &t.text) {
+                                words.push_str(w.as_ref());
+                            }
+                            let original_words = words.split_whitespace().collect::<Vec<_>>();
+                            let mut words = &original_words[..];
+                            let mut goodness = 0;
+                            while let Some((_, rest)) = rules.lock().unwrap().parse(words) {
+                                words = rest;
+                                goodness += 1;
+                            }
+                            // println!("{goodness:2}: {original_words:?}");
+                            if goodness > best {
+                                best = goodness;
+                                best_vec = original_words.iter().map(|w| w.to_string()).collect();
                             }
                         }
-
-                        if finished_phrase {
-                            silent_buffers = 0;
-                            last_length = 0;
-                            let old = std::mem::replace(&mut streaming, None);
-                            drop(old);
+                        let words = best_vec.iter().map(|w| w.as_str()).collect::<Vec<_>>();
+                        let mut words = &words[..];
+                        while let Some((a, rest)) = rules.lock().unwrap().parse(&words[..]) {
+                            assert!(rest.len() < words.len());
+                            words = rest;
+                            a.run();
                         }
                     }
+                    have_sound = false;
                 }
             },
             move |err| {
